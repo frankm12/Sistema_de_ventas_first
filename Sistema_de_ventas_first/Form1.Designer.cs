da1c78febcc6c1748b98acd850ddc0bd8f6e98e5
ALEJANDRO DAVID CAPELLAN SANCHEZ <alejandrocn63@gmail.com>
2024-07-30 19:10:08 -0400

    version 1.3

--- Sistema_de_ventas_first/Form1.Designer.cs
+++ Sistema_de_ventas_first/Form1.Designer.cs
@@ -28,19 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(195, 387);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(492, 670);
-            Margin = new Padding(4, 4, 4, 4);
+            Controls.Add(button1);
+            Margin = new Padding(4);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
     }
 }
