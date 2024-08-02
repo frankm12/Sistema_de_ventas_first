bca9b446e8e098e5512b0078d59b8c070fa161c7
frankm12 <reyapk234@gmail.com>
2024-08-02 15:36:55 -0400

    version 1.93

---
+++ Sistema_de_ventas_first/conexion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDConexion
+{
+    public class CDConexion
+    {
+        private SqlConnection Conexion = new SqlConnection("Server=.;DataBase= Prueba_de_sistema_de_ventas_1 ;Integrated Security=true");
+
+        public SqlConnection AbrirConexion()
+        {
+            if (Conexion.State == ConnectionState.Closed)
+                Conexion.Open();
+            return Conexion;
+        }
+
+        public SqlConnection CerrarConexion()
+        {
+            if (Conexion.State == ConnectionState.Open)
+                Conexion.Close();
+            return Conexion;
+        }
+    }
+}
