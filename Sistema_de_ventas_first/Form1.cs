f9008a6ecd164432573cee906bb252fedfe0f3c8
ALEJANDRO DAVID CAPELLAN SANCHEZ <alejandrocn63@gmail.com>
2024-08-02 16:15:12 -0400

    version 2.2

--- Sistema_de_ventas_first/Form1.cs
+++ Sistema_de_ventas_first/Form1.cs
@@ -1,8 +1,14 @@
+
+using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
 namespace Sistema_de_ventas_first
 {
-    //comentario para subir
     public partial class Form1 : Form
     {
+        private La_conect conexion = new La_conect();
         public Form1()
         {
             InitializeComponent();
@@ -10,23 +16,54 @@ namespace Sistema_de_ventas_first
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
         }
 
         private void btingresar_Click(object sender, EventArgs e)
         {
+            try
+            {
+                string query = "SELECT usuario, contraseña FROM usuarios_ingreso WHERE usuario = @usuario AND contraseña = @contraseña";
+                string usuario = textBox1.Text;
+                string contraseña = textBox2.Text;
+
+                La_conect conexion = new La_conect();
+                SqlConnection conexion_a_base_de_datos = conexion.AbrirConexion();
+                SqlCommand comando = new SqlCommand(query, conexion_a_base_de_datos);
+
+                comando.Parameters.AddWithValue("@usuario", usuario);
+                comando.Parameters.AddWithValue("@contraseña", contraseña);
+
+                SqlDataReader reader = comando.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    MessageBox.Show("Login Exitoso", "Sistema");
+                    Menu_princial principal = new Menu_princial();
+                    principal.Show();
+
+                    this.Hide();
+                    conexion.CerrarConexion();
+                }
+                else
+                {
+                    MessageBox.Show("Login Incorrecto", "Sistema");
+                    conexion.CerrarConexion();
+                }
 
 
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al ingresar datos: " + ex.Message);
+            }
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
+        }
 
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
         }
     }
 }
---
+++ Sistema_de_ventas_first/La_conect.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_ventas_first
+{
+    internal class La_conect
+    {
+        private SqlConnection Conexion = new SqlConnection("Server=.;DataBase=ventas;Integrated Security=true");
+
+        public SqlConnection AbrirConexion()
+        {
+            if (Conexion.State == ConnectionState.Closed)
+                Conexion.Open();
+            return Conexion;
+        }
+
+        public SqlConnection CerrarConexion()
+        {
+            if (Conexion.State == ConnectionState.Open)
+                Conexion.Close();
+            return Conexion;
+        }
+    }
+}
